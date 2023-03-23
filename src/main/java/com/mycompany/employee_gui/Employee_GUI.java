14c771324912d773cbc85e4dffb0432ba86f0880
JudeTulel <judeyego202@gmail.com>
2023-03-23 15:57:48 +0300

    Intial commit

---
+++ src/main/java/com/mycompany/employee_gui/Employee_GUI.java
@@ -0,0 +1,20 @@
+
+
+
+package com.mycompany.employee_gui;
+
+/**
+ *
+ * @author Jude Tulel
+ *
+ */
+
+public class Employee_GUI {
+
+    public static void main(String[] args) {
+       
+        Menu menu = new Menu();
+        menu.show();
+    }
+}
+
